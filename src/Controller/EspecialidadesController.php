8f4567d4aaa253cf4dec6e608464180a60950b3f
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-16 02:27:25 -0300

    feate: make route for delete

--- src/Controller/EspecialidadesController.php
+++ src/Controller/EspecialidadesController.php
@@ -83,4 +83,19 @@ class EspecialidadesController extends AbstractController
 
         return new JsonResponse($especialidade);
     }
+
+    #[Route("/especialidades/{id}", methods: ["DELETE"])]
+    public function remove(int $id): Response
+    {
+        $especialidade = $this->repository->find($id);
+
+        if (is_null($especialidade)) {
+            return new Response('',Response::HTTP_NOT_FOUND);
+        }
+
+        $this->entityManager->remove($especialidade);
+        $this->entityManager->flush();
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
 }
