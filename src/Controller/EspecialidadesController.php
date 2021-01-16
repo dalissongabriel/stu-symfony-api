93b30a27c55039c5a425f829999c6d27aa1cd1d2
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-16 01:45:37 -0300

    feat: add route for create especialidade

---
+++ src/Controller/EspecialidadesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Especialidade;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EspecialidadesController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/especialidades', methods: ["POST"])]
+    public function nova(Request $request): Response
+    {
+        $corpoRequisicao = $request->getContent();
+        $dadosEmJson = json_decode($corpoRequisicao);
+        $especialidade = new Especialidade();
+        $especialidade->setDescricao($dadosEmJson->descricao);
+
+        $this->entityManager->persist($especialidade);
+        $this->entityManager->flush();
+
+        return new JsonResponse($especialidade);
+    }
+}
