1fa9ab71f16ddcea0c575aaba48565723765c19b
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-16 01:18:12 -0300

    chore: alter migration for postgres usage

--- migrations/Version20210116041741.php
+++ migrations/Version20210116041741.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210116022558 extends AbstractMigration
+final class Version20210116041741 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,15 @@ final class Version20210116022558 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE medico (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome VARCHAR(255) NOT NULL, crm INTEGER NOT NULL)');
+        $this->addSql('CREATE SEQUENCE medico_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE medico (id INT NOT NULL, nome VARCHAR(255) NOT NULL, crm INT NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE medico_id_seq CASCADE');
         $this->addSql('DROP TABLE medico');
     }
 }
